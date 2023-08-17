ea64258fdd125c9518d97bb46f0275fe6e32583b
Brian Henning <henninb@msn.com>
2023-08-16 19:12:23 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          React Netlify Cloudfront
+          React Netlify Fastly
         </p>
       </header>
     </div>
